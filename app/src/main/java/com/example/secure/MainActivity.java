b8cebdd780a64eb628dcf421b007800592392262
Khush08 <khushdassani08@gmail.com>
2021-07-02 00:04:46 +0530

    Permissions functionality added

--- app/src/main/java/com/example/secure/MainActivity.java
+++ app/src/main/java/com/example/secure/MainActivity.java
@@ -13,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
     Intent encIntent;
     Intent signinIntent;
@@ -22,17 +24,29 @@ public class MainActivity extends AppCompatActivity {
     private static final String Key_key = "secureDesKey";
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    finishAffinity();
+                } else {
+                    finish();
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT >= 28){
-            if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED
-                    && ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
                 ActivityCompat.requestPermissions(MainActivity.this,
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-            }
         }
+
         setContentView(R.layout.activity_main);
         Button encBtn = findViewById(R.id.encBtn);
         Button decBtn = findViewById(R.id.decBtn);
@@ -52,6 +66,10 @@ public class MainActivity extends AppCompatActivity {
             signinIntent= new Intent(MainActivity.this, SignIn_Activity.class);
             startActivity(signinIntent);
         });
+
+
+
+
 
 
     }
